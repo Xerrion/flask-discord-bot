5ebd7fb616a66cbee36e0f0931506cbdd713b456
Lasse Nielsen <lassexl20@gmail.com>
2018-10-03 13:19:31 +0200

    Added  tests

---
+++ tests.py
@@ -0,0 +1,16 @@
+servers = {
+    'ru': 'ru',
+    'kr': 'kr',
+    'br': 'br1',
+    'oca': 'oc1',
+    'jp': 'jp1',
+    'na': 'na1',
+    'euw': 'euw1',
+    'eune': 'eun1',
+    'tr': 'tr1',
+    'la1': 'la1',
+    'la2': 'la2'
+}
+for s,d in servers.items():
+    print(f"server: {s}")
+    print(f"domain: {d}")
