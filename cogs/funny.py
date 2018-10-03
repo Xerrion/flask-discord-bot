730bf65d500b31166a6596a9e9746ae21450f441
Lasse Nielsen <lassexl20@gmail.com>
2018-10-03 13:19:55 +0200

    Seperated League of Legends commands to new cog

--- cogs/funny.py
+++ cogs/funny.py
@@ -1,19 +1,25 @@
-import os
 import random
-import discord
-import datetime
 
 from discord.ext import commands
 
+from app import Answers
+
 
 class FunCommands:
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['8ball'])
-    async def eightball(self, ctx, *, question: commands.clean_content):
-        """ Ask the magic 8ball """
-        await ctx.send('sut')
+    @commands.command(name='8ball',
+                      description="Answers a yes/no question.",
+                      brief="Answers from the beyond.",
+                      aliases=['eight_ball', 'eightball', '8-ball'],
+                      pass_context=True)
+    async def eight_ball(self, context):
+        a = Answers.query.all()
+        possible_responses = random.choice(a)
+        await context.send(f"{possible_responses}, {context.message.author.mention}")
+
 
 def setup(bot):
     bot.add_cog(FunCommands(bot))
