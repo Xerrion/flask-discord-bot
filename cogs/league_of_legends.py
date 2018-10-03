5004140fc1d52411588627baf957177c2a0d0f11
Lasse Nielsen <lassexl20@gmail.com>
2018-10-03 19:42:24 +0200

    Fixed command not working

--- cogs/league_of_legends.py
+++ cogs/league_of_legends.py
@@ -1,25 +1,25 @@
-import datetime
 import json
 import os
+import pprint
 
 import discord
-from discord.ext import commands
 import requests
+from discord.ext import commands
 
 
 class LolCommands:
     servers = {
-        'ru': 'ru',
-        'kr': 'kr',
-        'br': 'br1',
-        'oca': 'oc1',
-        'jp': 'jp1',
-        'na': 'na1',
-        'euw': 'euw1',
-        'eune': 'eun1',
-        'tr': 'tr1',
-        'la1': 'la1',
-        'la2': 'la2'
+        'ru': {'domain': 'ru'},
+        'kr': {'domain': 'kr'},
+        'br': {'domain': 'br1'},
+        'oca': {'domain': 'oc1'},
+        'jp': {'domain': 'jp1'},
+        'na': {'domain': 'na1'},
+        'euw': {'domain': 'euw1'},
+        'eune': {'domain': 'eun1'},
+        'tr': {'domain': 'tr1'},
+        'la1': {'domain': 'la1'},
+        'la2': {'domain': 'la2'},
     }
     response = None
 
@@ -29,23 +29,27 @@ class LolCommands:
     @commands.command(name='lolstatus',
                       description="Get an overview of lol status",
                       brief="Displays server status from Riots servers",
-                      aliases=['lol_status', 'lolonline'],
+                      aliases=['lol_status', 'lolonline', 'islolup', 'lolstat'],
                       pass_context=True)
     async def server_status(self, ctx, server=None):
-        if not server:
-            return await ctx.send('You need to specify a server')
-        for server, domain in self.servers.items():
-            if server == f'{server}':
+        for s, d in self.servers.items():
+            if not server:
+                return await ctx.send('You need to specify a server. eg. !lolstatus na')
+            if s == server:
                 self.response = requests.get(
-                    f"https://{domain}.api.riotgames.com/lol/status/v3/shard-data?api_key={os.environ.get('LOL_API_KEY')}")
+                    f"https://{d['domain']}.api.riotgames.com/lol/status/v3/shard-data?api_key={os.environ.get('LOL_API_KEY')}")
+                break
             else:
-                return await ctx.send('You need to specify a server')
-        data = self.response.json()
-        embed = discord.Embed(title=str(f'{data["name"]}'))
-        embed.set_thumbnail(url=ctx.bot.user.avatar_url)
-        for d in data['services']:
-            embed.add_field(name=str(f'{d["name"]}'), value=f'{d["status"].capitalize()}', inline=True)
-        await ctx.send(embed=embed)
+                pass
+        if self.response.status_code == 200:
+            data = self.response.json()
+            embed = discord.Embed(title=str(f'{data["name"]}'))
+            embed.set_thumbnail(url=ctx.bot.user.avatar_url)
+            for d in data['services']:
+                embed.add_field(name=str(f'{d["name"]}'), value=f'{d["status"].capitalize()}', inline=True)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send('There was an error contacting Riot Developer API')
 
 
 def setup(bot):
