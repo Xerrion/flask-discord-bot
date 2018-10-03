9ecb3426858f3fc330ca28d8a1d4a1b1b994d23e
Lasse Nielsen <lassexl20@gmail.com>
2018-10-03 11:53:10 +0200

    Now using f-strings

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ class User(db.Model):
         return self.name
 
     def __repr__(self):
-        return '<User {}>'.format(self.name)
+        return f'<User {self.name}>'
 
 
 class Answers(db.Model):
