bade96846d18fcc95231b4d5473958414d3cd4e6
Lasse Nielsen <lassexl20@gmail.com>
2018-10-03 19:42:32 +0200

    Added tests

--- tests.py
+++ tests.py
@@ -1,16 +1,24 @@
+import os
+
 servers = {
-    'ru': 'ru',
-    'kr': 'kr',
-    'br': 'br1',
-    'oca': 'oc1',
-    'jp': 'jp1',
-    'na': 'na1',
-    'euw': 'euw1',
-    'eune': 'eun1',
-    'tr': 'tr1',
-    'la1': 'la1',
-    'la2': 'la2'
+    'ru': {'domain': 'ru'},
+    'kr': {'domain': 'kr'},
+    'br': {'domain': 'br1'},
+    'oca': {'domain': 'oc1'},
+    'jp': {'domain': 'jp1'},
+    'na': {'domain': 'na1'},
+    'euw': {'domain': 'euw1'},
+    'eune': {'domain': 'eun1'},
+    'tr': {'domain': 'tr1'},
+    'la1': {'domain': 'la1'},
+    'la2': {'domain': 'la2'},
 }
-for s,d in servers.items():
-    print(f"server: {s}")
-    print(f"domain: {d}")
+server = 'eune'
+for s, d in servers.items():
+    # self.response = requests.get(
+    #    f"https://{d['domain']}.api.riotgames.com/lol/status/v3/shard-data?api_key={os.environ.get('LOL_API_KEY')}")
+    if s == server:
+        print(
+            f"https://{d['domain']}.api.riotgames.com/lol/status/v3/shard-data?api_key={os.environ.get('LOL_API_KEY')}")
+else:
+    pass
