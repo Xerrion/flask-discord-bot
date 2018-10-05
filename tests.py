7e5c360cae59cd4db890119983606a27f2735f37
Lasse Nielsen <lasse.nielsen@helphouse.io>
2018-10-05 15:18:12 +0200

    Added Cassiopia as new API Framework

--- tests.py
+++ tests.py
@@ -1,24 +1,9 @@
 import os
+import cassiopeia as lol
 
-servers = {
-    'ru': {'domain': 'ru'},
-    'kr': {'domain': 'kr'},
-    'br': {'domain': 'br1'},
-    'oca': {'domain': 'oc1'},
-    'jp': {'domain': 'jp1'},
-    'na': {'domain': 'na1'},
-    'euw': {'domain': 'euw1'},
-    'eune': {'domain': 'eun1'},
-    'tr': {'domain': 'tr1'},
-    'la1': {'domain': 'la1'},
-    'la2': {'domain': 'la2'},
-}
-server = 'eune'
-for s, d in servers.items():
-    # self.response = requests.get(
-    #    f"https://{d['domain']}.api.riotgames.com/lol/status/v3/shard-data?api_key={os.environ.get('LOL_API_KEY')}")
-    if s == server:
-        print(
-            f"https://{d['domain']}.api.riotgames.com/lol/status/v3/shard-data?api_key={os.environ.get('LOL_API_KEY')}")
-else:
-    pass
+lol.set_riot_api_key(os.environ.get('LOL_API_KEY'))
+lol.set_default_region('EUNE')
+
+summoner = lol.get_summoner(name='Xerrion')
+
+print("{name} is a level summoner on the {region} server.".format(name=summoner.name, region=summoner.region))
