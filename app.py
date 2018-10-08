c2ac77a2325edeeabf4cbb41cfa9dd3920b471ff
Lasse Nielsen <lasse.nielsen@helphouse.io>
2018-10-08 09:49:31 +0200

    Made database file changeable

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Let's create a database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.environ.get('DATABASE_FILE'))
 db = SQLAlchemy(app)
 
 
