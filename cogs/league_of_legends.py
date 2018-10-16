42ab1912ae4d83f3bdc5e7033f65458d920cdcb4
Lasse Nielsen <lassexl20@gmail.com>
2018-10-16 15:46:12 +0200

    Fixed summoner command not sending

--- cogs/league_of_legends.py
+++ cogs/league_of_legends.py
@@ -4,28 +4,15 @@ from discord import Embed
 from discord.ext import commands
 from requests import HTTPError
 from riotwatcher import RiotWatcher
-from pprint import pprint
 
 import settings
 from utils.regions import get_region
 
 
 class LolCommands:
-    regions = {
-        'ru': {'domain': 'ru'},
-        'kr': {'domain': 'kr'},
-        'br': {'domain': 'br1'},
-        'oca': {'domain': 'oc1'},
-        'jp': {'domain': 'jp1'},
-        'na': {'domain': 'na1'},
-        'euw': {'domain': 'euw1'},
-        'eune': {'domain': 'eun1'},
-        'tr': {'domain': 'tr1'},
-        'la1': {'domain': 'la1'},
-        'la2': {'domain': 'la2'},
-    }
     watcher = RiotWatcher(settings.RIOT_API_KEY)
     domain = None
+    lol_icon = 'https://i.imgur.com/1qe880q.png'
 
     def __init__(self, bot):
         self.bot = bot
@@ -49,30 +36,44 @@ class LolCommands:
                 await ctx.send('Region Not Found')
         except HTTPError as error:
             if error.response.status_code == 401:
-                pass
+                await ctx.send('Error 401 - Unauthorized')
+            if error.response.status_code == 403:
+                await ctx.send('Error 403 - Forbidden')
             if error.response.status_code == 404:
-                await ctx.send('not found')
+                await ctx.send('Error 404 - Not Found')
             else:
                 pass
 
     @commands.command(name='summoner',
                       brief='Get details about a summoner from a specific region. Usage: !summoner REGION SUMMONER')
-    async def summoner_info(self, ctx, region, summoner):
-        try:
-            if get_region(region):
-                user = self.watcher.summoner.by_name(region=get_region(region), summoner_name=summoner)
-                league = self.watcher.league.positions_by_summoner(get_region(region), user['id'])
-                if league[0]['queueType'] == 'RANKED_SOLO_5x5':
-                    queue_type = 'Solo/Duo'
-                embed = Embed(title=f'{queue_type}')
-                embed.set_thumbnail(url=
-                                    'http://www.macupdate.com/images/icons256/47210.png')
-                embed.add_field(name='Test', value='Tester')
-                embed.set_footer(text='Generated')
-                await ctx.send(embed=embed)
-        except HTTPError as error:
-            if error.response.status_code == 404:
-                await ctx.send('Summoner not found')
+    async def summoner_info(self, ctx, region=None, summoner=None):
+        if not region and not summoner:
+            await ctx.send('You need to specify a region and a summoner')
+        elif not region:
+            await ctx.send('You need to specify a region')
+        elif not summoner:
+            await ctx.send('You need to specify a summoner')
+        elif not get_region(region):
+            await ctx.send('Region does not exist')
+        else:
+            try:
+                if get_region(region):
+                    user = self.watcher.summoner.by_name(region=get_region(region), summoner_name=summoner)
+                    leagues = self.watcher.league.positions_by_summoner(get_region(region), user['id'])
+                    embed = Embed(title=f'{user["name"]}')
+                    embed.set_thumbnail(
+                        url=f'http://ddragon.leagueoflegends.com/cdn/8.20.1/img/profileicon/{user["profileIconId"]}.png')
+                    for league in leagues:
+                        if league['queueType'] == 'RANKED_SOLO_5x5':
+                            queue_type = 'Solo/Duo'
+                        if league['queueType'] == 'RANKED_FLEX_SR':
+                            queue_type = 'Flex'
+                    embed.add_field(name='Level', value=f'{user["summonerLevel"]}')
+                    embed.set_footer(text='Generated')
+                    await ctx.send(embed=embed)
+            except HTTPError as error:
+                if error.response.status_code == 404:
+                    await ctx.send('Summoner not found')
 
 
 def setup(bot):
